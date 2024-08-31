2e682e5da863d83bc19cd3ca788831c84a9cfb3d
Thalles Silva <98830931+Thalles-JG-Silva@users.noreply.github.com>
2024-08-31 20:28:52 -0300

    Exercicio finalizado

---
+++ ex004.py
@@ -0,0 +1,12 @@
+a = input('Digite o valor: ')
+print('O tipo primitivo desse valor é ', type(a))
+print('Só tem espaços? ', a.isspace())
+print('É um numero? ', a.isnumeric())
+print('É alfabético? ', a.isalpha())
+print('É alfanumerico? ', a.isalnum())
+print('Está em maiúsculas? ', a.isupper())
+print('Está em minúsculas? ', a.islower())
+print('Está capitalizada? ', a.istitle())
+
+
+
