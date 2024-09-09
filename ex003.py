3499285510ed2fbe7b2b482e23b34627422fad76
Thalles Silva <98830931+Thalles-JG-Silva@users.noreply.github.com>
2024-09-09 17:48:51 -0300

    Finalizando ex

--- ex003.py
+++ ex003.py
@@ -1,3 +1,5 @@
+#crie um programa que leia dois números e mostre a soma entre eles.
+
 n1 = int(input('Digite um valor: '))
 n2 = int(input('Digite outro valor: '))
 
---
+++ ex005.py
@@ -0,0 +1,6 @@
+#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
+
+n = int(input("Digite um numero: "))
+
+print("O sucessor do número é {} e seu antecessor é {}.".format(n+1, n-1))
+
